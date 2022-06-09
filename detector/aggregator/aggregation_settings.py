df3691e51983244e867a0b7344f2bd46150597e1
Anton Petrov <anton@Antons-MacBook-Pro.local>
2022-06-09 16:08:55 +0300

    added pre aggregation

--- detector/aggregator/aggregation_settings.py
+++ detector/aggregator/aggregation_settings.py
@@ -74,15 +74,24 @@ AGGREGATION_SETTINGS = AggregationSetting(
             lambda x: case(
                 [
                     (
-                        and_(extract_time(func.max(x.dttm)) >= "00:00:00", extract_time(func.max(x.dttm)) < "06:00:00"),
+                        and_(
+                            extract_time(func.max(x.max_dttm)) >= "00:00:00",
+                            extract_time(func.max(x.max_dttm)) < "06:00:00",
+                        ),
                         0,
                     ),
                     (
-                        and_(extract_time(func.max(x.dttm)) >= "06:00:00", extract_time(func.max(x.dttm)) < "12:00:00"),
+                        and_(
+                            extract_time(func.max(x.max_dttm)) >= "06:00:00",
+                            extract_time(func.max(x.max_dttm)) < "12:00:00",
+                        ),
                         1,
                     ),
                     (
-                        and_(extract_time(func.max(x.dttm)) >= "12:00:00", extract_time(func.max(x.dttm)) < "18:00:00"),
+                        and_(
+                            extract_time(func.max(x.max_dttm)) >= "12:00:00",
+                            extract_time(func.max(x.max_dttm)) < "18:00:00",
+                        ),
                         2,
                     ),
                 ],
--- detector/aggregator/aggregator.py
+++ detector/aggregator/aggregator.py
@@ -29,20 +29,34 @@ class Aggregator:
             )
 
     def _get_aggregate_query(self, dttm: datetime, session: Session, raw_value_cls: BaseRawValue) -> Query:
+        dttm_from = dttm - timedelta(minutes=self.period_length) + timedelta(seconds=2)
+        dttm_to = dttm
+        average_fields = ["cpu_percent", "memory_percent", "num_threads", "connections", "open_files"]
+        groupped_processes_qs = (
+            session.query(
+                raw_value_cls.status,
+                raw_value_cls.username,
+                *[func.avg(getattr(raw_value_cls, field)).label(field) for field in average_fields],
+                func.max(raw_value_cls.dttm).label("max_dttm"),
+            )
+            .filter(
+                raw_value_cls.dttm > dttm_from,
+                raw_value_cls.dttm <= dttm_to,
+            )
+            .group_by("pid", "username", "status")
+            .subquery()
+        )
         aggregation_settings = [
-            getattr(func, simple_agg.func_name)(getattr(raw_value_cls, field)).label(f"{field}_{simple_agg.func_name}")
+            getattr(func, simple_agg.func_name)(getattr(groupped_processes_qs.c, field)).label(
+                f"{field}_{simple_agg.func_name}"
+            )
             for simple_agg in self.aggregation_settings.simple_agg
             for field in simple_agg.fields
         ] + [
-            custom_agg.func(raw_value_cls).label(custom_agg.label)
+            custom_agg.func(groupped_processes_qs.c).label(custom_agg.label)
             for custom_agg in self.aggregation_settings.custom_agg
         ]
-        dttm_from = dttm - timedelta(minutes=self.period_length) + timedelta(seconds=2)
-        dttm_to = dttm
-        return session.query(*aggregation_settings).filter(
-            raw_value_cls.dttm > dttm_from,
-            raw_value_cls.dttm <= dttm_to,
-        )
+        return session.query(*aggregation_settings)
 
     def get_detect_data(self, dttm: datetime) -> pd.DataFrame:
         with session_scope() as session:
